0dd53e739dce01a8e012f486c231bff6974a01e7
Pavel Khrebto <rezgo.dev@dataart.com>
2018-03-16 20:11:39 +0100

    ST-13 simple auth system

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import { Actions, Router, Stack, Scene } from 'react-native-router-flux';
 import { CountryScreen, LoginScreen, MainScreen, MapScreen } from './screens';
 
 class App extends Component {
-    isLoggedIn() {
+    authCheck() {
         let isAuth = false;
-        if (isAuth === true) {
-            // what to do?
-        } else {
+        if (isAuth === false) {
             Actions.login();
         }
     }
+
+    loginAuthCheck() {
+        if (isAuth === true) {
+            Actions.main();
+        }
+    }
+
     render() {
         return (
             <Router>
@@ -21,20 +26,21 @@ class App extends Component {
                         key="main"
                         component={MainScreen}
                         title="Main"
-                        onEnter={this.isLoggedIn}
+                        onEnter={this.authCheck}
                     />
                     <Scene
                         key="login"
                         component={LoginScreen}
                         title="Login"
                         hideNavBar={true}
+                        onEnter={this.loginAuthCheck}
                     />
                     <Scene
                         key="country"
                         component={CountryScreen}
-                        onEnter={this.isLoggedIn}
+                        onEnter={this.authCheck}
                     />
-                    <Scene key="map" component={MapScreen} onEnter={this.isLoggedIn} />
+                    <Scene key="map" component={MapScreen} onEnter={this.authCheck} />
                 </Stack>
             </Router>
         );
