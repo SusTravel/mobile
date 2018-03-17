8b181e22443f933c0ac7e6387a1ca0aa78798f67
Pavel Khrebto <rezgo.dev@dataart.com>
2018-03-17 12:29:48 +0100

    ST-4 final make maps work

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
+import MapView from 'react-native-maps';
 import { connect } from 'react-redux';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
+const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+  });
+  
 class MapScreen extends Component {
     componentDidMount() {
         this.continentName = Actions.state.routes[Actions.state.index].params.name;
 
-        navigator.geolocation.getCurrentPosition(
-            console.log,
-            console.log
-        );
+        navigator.geolocation.getCurrentPosition(console.log, console.log);
     }
     render() {
         return (
-            <View>
-                <Text>Privet</Text>
-            </View>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}
+            />
         );
     }
 }
